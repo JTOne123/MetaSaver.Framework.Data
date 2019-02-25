9a15c27f0392b131ba1e1a3abc492a33ef24a114
Jim Nightingale <jim@metasaver.com>
2019-02-25 18:13:47 -0500

    Formatting

--- src/MetaSaver.Framework.Data/ContextManager.cs
+++ src/MetaSaver.Framework.Data/ContextManager.cs
@@ -9,31 +9,37 @@ using Polly.Retry;
 namespace MetaSaver.Framework.Data
 {
     #region Interface
+
     public interface IContextManager : IDisposable
     {
         IDbConnection Connection { get; }
     }
+
     #endregion
 
     #region Class
+
     public class ContextManager : IContextManager
     {
         #region Locals
+
         private readonly RetryPolicy _policy;
         private IDbConnection _connection;
         private readonly object _syncObject = new object();
         private string ConnectionString { get; }
+
         #endregion
 
         #region Constructor
+
         public ContextManager(IOptions<BaseAppSettings> appSettings)
         {
             ConnectionString = appSettings.Value.Connections.Database;
 
             _policy = Policy.Handle<Exception>().WaitAndRetry(
-                retryCount: 10,
-                sleepDurationProvider: attempt => TimeSpan.FromSeconds(0.1 * Math.Pow(2, attempt)),
-                onRetry: (exception, calculatedWaitDuration) =>
+                10,
+                attempt => TimeSpan.FromSeconds(0.1 * Math.Pow(2, attempt)),
+                (exception, calculatedWaitDuration) =>
                 {
                     GC.Collect(0);
                     GC.Collect(1);
@@ -41,9 +47,11 @@ namespace MetaSaver.Framework.Data
                 }
             );
         }
+
         #endregion
 
         #region Connection
+
         public IDbConnection Connection
         {
             get
@@ -69,14 +77,18 @@ namespace MetaSaver.Framework.Data
                 }
             }
         }
+
         #endregion
 
         #region Dispose
+
         public void Dispose()
         {
             _connection?.Dispose();
         }
+
         #endregion
     }
+
     #endregion
 }
